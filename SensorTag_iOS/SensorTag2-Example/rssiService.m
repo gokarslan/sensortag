30d64937210c3efe6dd2856c63ae6c6c6f28be7b
Kerim Gökarslan <kerimgokarslan@Kerims-MacBook-Air.local>
2016-06-16 10:27:18 +0300

    added rssi

---
+++ SensorTag_iOS/SensorTag2-Example/rssiService.m
@@ -0,0 +1,9 @@
+//
+//  rssiService.m
+//  SensorTag2-Example
+//
+//  Created by Kerim Gökarslan on 6/15/16.
+//  Copyright © 2016 Texas Instruments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
